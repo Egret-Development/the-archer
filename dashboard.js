cff5549b888ddb1fc2bd3af7f2007b3a34827928
hydrogen-studio <egret.support@virtualegret.com>
2023-02-19 21:12:42 -0800

    test

--- dashboard.js
+++ dashboard.js
@@ -23,6 +23,16 @@ app.get('/invite', function(req, res) {
 	res.status(308).redirect('https://discord.com/api/oauth2/authorize?client_id=1076722106684952616&permissions=1642691165303&scope=bot%20applications.commands');
 });
 
+// route for redirect
+app.get('/invite', function(req, res) {
+	res.status(308).redirect('https://discord.com/api/oauth2/authorize?client_id=1076722106684952616&permissions=1642691165303&scope=bot%20applications.commands');
+});
+
+// route for login page
+app.get('/login', function(req, res) {
+	res.status(308).redirect('https://discord.com/api/oauth2/authorize?client_id=1076722106684952616&redirect_uri=https%3A%2F%2Farcher.egretdevelopment.com%2Fredirect&response_type=code&scope=identify%20guilds');
+});
+
 
 app.listen(80, function() {
 	console.log('Server is running on port 80 ');
