603319c503a5de133ede62b79f9820e1789d502d
hydrogen-studio <egret.support@virtualegret.com>
2023-02-21 13:08:33 -0800

    ees

--- dashboard.js
+++ dashboard.js
@@ -82,7 +82,7 @@ app.get('/redirect', async function(req, res) {
     if (token.error) return res.redirect('/login');
     let data = await login(res, token);
     console.log(data)
-    if(data.status == 200) return res.redirect('/dashboard');
+    if(data.status == 200) return res.send('/test');
 });
 
 async function login(res, token) {
